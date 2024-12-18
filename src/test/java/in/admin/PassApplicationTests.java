39caee4d41addc05f92f100c5542c1a787d9bd11
nthokal24 <nthokal3@gmail.com>
2024-12-19 01:08:53 +0530

    Hi

---
+++ src/test/java/in/admin/PassApplicationTests.java
@@ -0,0 +1,13 @@
+package in.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
